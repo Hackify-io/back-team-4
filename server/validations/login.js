2707672d3972200e3f6db90f09145b0df4d1a2ed
Gustavo Adolfo Loera Vazquez <gustavo.loera@softtek.com>
2020-01-18 15:14:41 -0800

    added login validation helper

---
+++ server/validations/login.js
@@ -0,0 +1,22 @@
+import validator from 'validator';
+import { isEmpty, isRequired } from './is-empty';
+
+export const validateLoginInput = data => {
+  let errors = {};
+
+  data.username = isRequired(data.username);
+  data.password = isRequired(data.password);
+
+  if (validator.isEmpty(data.username)) {
+    errors.username = 'Username field is required';
+  }
+
+  if (validator.isEmpty(data.password)) {
+    errors.password = 'Password field is required';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
