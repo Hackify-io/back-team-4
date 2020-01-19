0dc41978aa5ba0ea07c85d1615aa77083571e9d6
Gustavo Adolfo Loera Vazquez <gustavo.loera@softtek.com>
2020-01-18 17:16:45 -0800

    register endpoints done without validations

--- server/routes/login.js
+++ server/routes/login.js
@@ -6,38 +6,37 @@ import passport from 'passport';
 const router = express.Router();
 
 //Load Input Validation
-import { validateRegisterInput } from './../validations/register';
+//TODO
+//validate register inputs
 import { validateLoginInput } from './../validations/login';
 //Load Models
 import Login from '../models/Login';
 import ApiResponse from '../models/ApiResponse';
 
-// @route   GET API/logins/register
+//import constants
+import { roles } from '../constants/constants';
+
+// @route   POST api/logins/clinics/register
 // @desc    Register new Login
 // @access  Public
-router.post('/register', async (req, res) => {
+router.post('/clinics/register', async (req, res) => {
   let response = new ApiResponse();
   const register = req.body;
-  const { errors, isValid } = validateRegisterInput(register);
+  //TODO
+  //body validations
 
-  //Check Model Validation Errors
-  if (!isValid) {
-    await response.ValidationError(errors);
-    return res.status(response.statusCode).json(response);
-  }
-
-  let getLoginRequest = await Login.findOne({ username: register.username });
+  let getLoginRequest = await Login.findOne({ email: register.email });
+  //clinic login already exists
   if (getLoginRequest) {
-    errors.username = `Username ${getLoginRequest.username} already exist`;
+    errors.email = `Email ${getLoginRequest.email} is already registered`;
     await response.ValidationError(errors);
     return res.status(response.statusCode).json(response);
   }
 
   let newLogin = new Login({
-    username: register.username,
     email: register.email,
-    password: register.password
-    // role = register.role //get user role from constants file
+    password: register.password,
+    role: roles.clinic
   });
 
   bcrypt.genSalt(10, (err, salt) => {
@@ -59,10 +58,54 @@ router.post('/register', async (req, res) => {
   });
 });
 
-// @route   GET API/users/login
-// @desc    Login user: Returning a JWT
+// @route   POST api/logins/users/register
+// @desc    Register new Login
 // @access  Public
-router.post('/login', async (req, res) => {
+router.post('/users/register', async (req, res) => {
+  let response = new ApiResponse();
+  const register = req.body;
+  //TODO
+  //validations
+
+  let getLoginRequest = await Login.findOne({ email: register.email });
+  //clinic login already exists
+  if (getLoginRequest) {
+    //Temporal declaration
+    let errors = {};
+    errors.email = `Email ${getLoginRequest.email} is already registered`;
+    await response.ValidationError(errors);
+    return res.status(response.statusCode).json(response);
+  }
+
+  let newLogin = new Login({
+    email: register.email,
+    password: register.password,
+    role: roles.member
+  });
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      response.InternalServerError(err).then(() => {
+        return res.status(response.statusCode).json(response);
+      });
+    }
+
+    bcrypt.hash(newLogin.password, salt, (err, hash) => {
+      if (err) throw err;
+      newLogin.password = hash;
+      let loginResult = newLogin.save().then(() => {
+        response.Ok(loginResult).then(() => {
+          return res.status(response.statusCode).json(response);
+        });
+      });
+    });
+  });
+});
+
+// @route   POST api/logins/clinic
+// @desc    Login user of type clinic: Returning a JWT
+// @access  Public
+router.post('/clinics', async (req, res) => {
   let response = new ApiResponse();
 
   const loginRequest = req.body;
@@ -76,7 +119,7 @@ router.post('/login', async (req, res) => {
 
   //Look if Login exists
   let getLoginResponse = await Login.findOne({
-    username: loginRequest.username
+    email: loginRequest.email
   });
   if (!getLoginResponse) {
     await response.NotFound();
@@ -90,8 +133,7 @@ router.post('/login', async (req, res) => {
     //Sign the Token
     const payload = {
       id: login.coorelationId,
-      email: login.email,
-      username: login.username
+      email: login.email
     };
 
     jwt.sign(
@@ -114,20 +156,59 @@ router.post('/login', async (req, res) => {
   }
 });
 
-// @route   GET API/users/current
-// @desc    Register user
-// @access  Private
-router.get(
-  '/current',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    res.json({
-      id: req.user.id,
-      name: req.user.username,
-      email: req.user.email,
-      role: req.user.role
-    });
+// @route   POST api/logins/users
+// @desc    Login user: Returning a JWT
+// @access  Public
+router.post('/users', async (req, res) => {
+  let response = new ApiResponse();
+
+  const loginRequest = req.body;
+  //TODO
+  // const { errors, isValid } = validateLoginInput(loginRequest);
+
+  //Check Model Validation Errors
+  // if (!isValid) {
+  //   await response.ValidationError(errors);
+  //   return res.status(response.statusCode).json(response);
+  // }
+
+  //Look if Login exists
+  let getLoginResponse = await Login.findOne({
+    email: loginRequest.email
+  });
+  if (!getLoginResponse) {
+    await response.NotFound();
+    return res.status(response.statusCode).json(response);
   }
-);
+
+  //If login exist encrypt password and validate model
+  const login = getLoginResponse;
+  const isMatch = await bcrypt.compare(loginRequest.password, login.password);
+  if (isMatch) {
+    //Sign the Token
+    const payload = {
+      id: login.coorelationId,
+      email: login.email
+    };
+
+    jwt.sign(
+      payload,
+      keys.authSecret,
+      {
+        expiresIn: 3600,
+        audience: 'All',
+        issuer: 'medtravel'
+      },
+      (err, token) => {
+        if (err) {
+          response.InternalServerError(err);
+          return res.status(response.statusCode).json(response);
+        }
+        response.Ok(token);
+        res.status(response.statusCode).json(response);
+      }
+    );
+  }
+});
 
 export default router;
