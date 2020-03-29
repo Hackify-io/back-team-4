fdf64f37493a90b33a4159fdcfea29653921f1f6
Nibban8 <al18760117@ite.edu.mx>
2020-03-28 19:33:18 -0700

    Tried ClinicsReview PUSH

--- server/models/ClinicReview.js
+++ server/models/ClinicReview.js
@@ -1,22 +1,22 @@
 import mongoose from "mongoose";
 import VersionSchema from "./VersionSchema";
-import { Schema } from 'mongoose';
+import { Schema } from "mongoose";
 //Create Schema
 const ClinicReviewSchema = new VersionSchema({
-    clinicId: {
-        type: Schema.Types.ObjectId,
-        ref: 'clinics',
-        required: true
-      }, 
-      userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'users',
-        required: true
-      },
-      review:{
-          "username": 'string',
-          "message": 'string'
-      }
+  clinic: {
+    type: Schema.Types.ObjectId,
+    ref: "clinics",
+    required: true
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users",
+    required: true
+  },
+  review: {
+    username: "string",
+    message: "string"
+  }
 });
 
 const ClinicReview = mongoose.model("clinicreviews", ClinicReviewSchema);
--- server/routes/clinics.js
+++ server/routes/clinics.js
@@ -9,6 +9,46 @@ import Repository from "./../services/repository";
 import Clinic from "../models/Clinic";
 import { validateClinicFields } from "../validations/clinic";
 
+import ClinicReview from "../models/ClinicReview";
+
+// @route   POST api/clinics/:id/reviews
+// @desc    Add review to clinic
+// @access  Private
+router.post("/:clinicId/reviews/", async (req, res) => {
+  //Get Prerequirments: Clinic by ClinicId
+  try {
+    let clinic = await Clinic.findById(req.params.clinicId);
+
+    if (!clinic) {
+      await response.NotFound();
+      return res.status(response.statusCode).json(response);
+    }
+
+    //console.log(req.body);
+
+    const newReview = new ClinicReview({
+      modifiedUser: req.body.modifiedUser,
+      username: req.body.username,
+      message: req.body.message
+    });
+
+    clinic.reviews.push(newReview._id);
+    const response = await Repository.update(
+      Clinic,
+      clinic._id,
+      clinic.reviews,
+      validateClinicFields
+    );
+
+    console.log(response);
+
+    return res.status(response.statusCode).json(response);
+  } catch (err) {
+    await response.InternalServerError(err.message);
+    res.status(response.statusCode).json(response);
+  }
+});
+
 // @route   GET api/clinics?{filters}
 // @desc    Get clinics using filter
 // @access  Public
@@ -35,7 +75,7 @@ router.get("/:id", async (req, res) => {
       .populate("location");
     if (!clinic) {
       await response.NotFound();
-      return res.status(response.statusCode).json(response);
+      res.status(response.statusCode).json(response);
     }
 
     await response.Ok(clinic);
